b46b6766662371c0184f1fb6f24b847d957d76e3
Ben <nimajnebs@gmail.com>
2013-10-10 21:22:39 -0700

    Start exercise series

--- fabfile.py
+++ fabfile.py
@@ -1,15 +1,7 @@
 from fabric.api import local
 
-def run(mode="normal"):
-    """ start the local app server """
-    local("dev_appserver.py .")
-
-def save(m="Fab-update the app"):
+def update(m="Auto-update the app"):
     """ save the to github """
     local("git add .")
     local("git commit -a -m '{0}'".format(m))
-    local("git push")
 
-def update(app_id="charles-lindsay", version="2-2"):
-    """ upload the app """
-    local("appcfg.py --oauth2 update .")
